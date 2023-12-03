7459e34ba621540652a8661cfee08642ade99870
Henrique de Souza <enriquedesuu@gmail.com>
2023-12-03 09:41:01 -0300

    adicionei um requirements.txt e online

--- projeto_invista_me/settings.py
+++ projeto_invista_me/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'projeto_invista_me.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'Y4rowBWaN6nQr2NI',
+        'HOST':  'db.fsmtsjzdeaoiacxmzpdr.supabase.co',
+        'PORT': '5432'
     }
 }
 
